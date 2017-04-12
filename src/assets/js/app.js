33089f2de9b1b6a2d8e9864e9f44c023dfce37e2
Demian Ortega <demian.ortega.m@gmail.com>
2017-04-12 21:57:39 +0200

    jaavscript

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,30 @@
 $(document).foundation();
+
+  window.onload=function() {horloge('div_horloge');};
+    function horloge(el) {
+      if(typeof el=="string") { el = document.getElementById(el); }
+      function actualiser() {
+        var date = new Date();
+        var str = date.getHours();
+        str += ':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
+        str += ':'+(date.getSeconds()<10?'0':'')+date.getSeconds();
+        el.innerHTML = str;
+      }
+      actualiser();
+      setInterval(actualiser,1000);
+  }
+
+$(function() {
+    var d = new Date();
+    var currentHour = d.getHours();
+  $(".bar_fiche").each(function() {
+    var fromHour = $(this).data('from');
+    var toHour = $(this).data('to');
+    var from = fromHour.replace("h", "");
+    var to = toHour.replace("h", "");
+    if (currentHour > from && currentHour < to) {}
+    else{
+      $(this).addClass('ferme');
+    }
+  });
+});
